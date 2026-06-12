9fb115ed9fa515b86a2d86d2b2109356b26e454a
Huyen <nguyennguyen2004555@gmail.com>
2026-06-12 17:49:47 +0700

    Fix ImproperlyConfigured for superuser creation script

--- reqsys/build.sh
+++ reqsys/build.sh
@@ -12,6 +12,7 @@ echo "Checking and creating superuser if needed..."
 python -c "
 import os
 import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reqsys.settings')
 django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
